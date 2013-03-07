2924e2cbae229f8294819b673020338eaf36df5e
Noah Kurinsky <nkurinsky91@gmail.com>
2013-03-06 23:34:12 -0500

    commit test

--- Fitting/v0/make_fit_noah.sh
+++ Fitting/v0/make_fit_noah.sh
@@ -1,3 +1,11 @@
-g++ -c fit_mcmc.cpp simulator.cpp lumfunct.cpp functions.cpp hist_lib.cpp model_lib.cpp 
+g++ -c simulator.cpp 
+g++ -c lumfunct.cpp 
+g++ -c functions.cpp 
+g++ -c hist_lib.cpp 
+g++ -c sed_lib.cpp 
 
-g++ -g -lgsl -lcfitsio -lccfits fit_mcmc.o simulator.o lumfunct.o hist_lib.o model_lib.o -o fitter #-Wall -Werror -fexceptions -lgslcblas -O3
+echo "
+main build
+"
+
+g++ -g -lgsl -lcfitsio -lccfits fit_mcmc.cpp simulator.o lumfunct.o hist_lib.o sed_lib.o -o fitter #-Wall -Werror -fexceptions -lgslcblas -O3
