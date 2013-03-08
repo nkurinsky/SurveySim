a46af78547625920b8ff7f0a8672c893911d24fc
Noah Kurinsky <nkurinsky91@gmail.com>
2013-03-07 23:25:07 -0500

    Updated compilation script

--- Fitting/v0/make_fit_noah.sh
+++ Fitting/v0/make_fit_noah.sh
@@ -11,13 +11,47 @@ fi
 
 printf "Building Lumfunct.cpp..." 
 g++ -c lumfunct.cpp 
+if [ "$?" = "0" ]; then
+    printf "Successful \n"
+else
+    printf "\nLumfunct.cpp build failed...exiting\n\n" 1>&2
+    exit 1
+fi
 
+printf "Building Functions.cpp..." 
 g++ -c functions.cpp 
+if [ "$?" = "0" ]; then
+    printf "Successful \n"
+else
+    printf "\nFunctions.cpp build failed...exiting\n\n" 1>&2
+    exit 1
+fi
+
+printf "Building hist_lib.cpp..." 
 g++ -c hist_lib.cpp 
+if [ "$?" = "0" ]; then
+    printf "Successful \n"
+else
+    printf "\nhist_lib.cpp build failed...exiting\n\n" 1>&2
+    exit 1
+fi
+
+printf "Building sed_lib.cpp..." 
 g++ -c sed_lib.cpp 
+if [ "$?" = "0" ]; then
+    printf "Successful \n"
+else
+    printf "\nsed_lib.cpp build failed...exiting\n\n" 1>&2
+    exit 1
+fi
 
-echo "
-main build
-"
 
+printf "\nBuilding Main..."
 g++ -g -lgsl -lcfitsio -lccfits fit_mcmc.cpp simulator.o lumfunct.o hist_lib.o sed_lib.o -o fitter #-Wall -Werror -fexceptions -lgslcblas -O3
+if [ "$?" = "0" ]; then
+    printf "Successful \n"
+    exit 0
+else
+    printf "\nsed_lib.cpp build failed...exiting\n\n" 1>&2
+    exit 1
+fi
