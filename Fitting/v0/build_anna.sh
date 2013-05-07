5df24b1a2afb8ed91989e1f9c2dedd060cc8ce8a
Noah Kurinsky <nkurinsky91@gmail.com>
2013-05-07 14:15:53 -0400

    Function Checking

--- Fitting/v0/build_anna.sh
+++ Fitting/v0/build_anna.sh
@@ -63,7 +63,6 @@ printf "\nBuilding Main..."
 g++ -g -lgsl -/scisoft/CCfits/include/CCfits -/scisoft/cfitsio/lcfitsio -lccfits fit_mcmc.cpp simulator.o lumfunct.o hist_lib.o sed_lib.o obs_lib.o functions.o -o fitter -Wall -Werror -fexceptions -lgslcblas -O3
 if [ "$?" = "0" ]; then
     printf "Successful \n"
-    exit 0
 else
     printf "\nMain build failed...exiting\n\n" 1>&2
     exit 1
