2d29e60e72f5e60a469a67d1b46ea874785576cb
asajina <annie.sajina@gmail.com>
2014-01-27 18:16:08 -0500

    changes_Jan27
    some more edits on run.py

--- run.py
+++ run.py
@@ -7,11 +7,19 @@ import time
 from astropy.io import fits
 
 import os
-thisdir=os.getcwd();
-sedfile=thisdir+'/Widget/sf_templates.fits'
-modelfile=thisdir+'/Widget/model.fits'
-obsfile=thisdir+'/Widget/observation.fits'
-outfile=thisdir+'/Widget/output.fits'
+codedir=os.getcwd();
+#if want to be able to run this from wherever, should simply change the codedir 
+sedfile=codedir+'/Widget/sf_templates.fits'
+modelfile=codedir+'/Widget/model.fits'
+obsfile=codedir+'/Widget/observation.fits'
+outfile=codedir+'/Widget/output.fits'
+
+def runcode():
+    print("Runnning fitter code....")
+    os.system(codedir+"/Fitting/v0/fitter"+' '+obsfile+' '+modelfile+' '+sedfile+' '+outfile)
+
+def showresults():
+    print("Showing results....")
 
 defaults=raw_input("Do you wish to see/edit the default file settings (y/n)?");
 if (defaults == 'y'):
@@ -36,10 +44,10 @@ mdefaults=raw_input("Do you wish to see/edit the default model settings (y/n)?")
 #initialize luminosity function parameters
 fields='Phi_0','Lstar_0','alpha','beta','p','q','zcut','c_evol'
 value_initial=[-2.20,10.14,0.50,3.00,-4.50,4.50,2.00,0.0]
-value_min=[-5.00,9.00,0.00,0.00,-8.00,3.00,0.00,0.0]
-value_max=[5.00,11.00,2.00,5.00,-1.00,6.00,0.00,1.0]
-value_fix=[1,1,1,1,0,0,1,0]
-#not sure why we need this????
+value_min=[-3.00,9.00,0.00,0.00,-8.00,3.00,0.00,0.0]
+value_max=[-1.00,11.00,2.00,5.00,-1.00,6.00,0.00,1.0]
+value_fix=[1,1,1,1,0,0,1,1]
+#not sure why we need this here, I feel that the sigma of the proposal distribution shouldn't be user controllable
 value_dp=[0.0,0.0,0.0,0.0,0.3,0.1,0.0,0.0]
 
 #initialize survey parameters
@@ -47,6 +55,82 @@ area=[4.0,4.0,4.0]
 wavelength=[250,350,500]
 flim=[25.0,20.0,15.0]
 
+def update_mfile(modelfile):
+    hdu1=fits.open(modelfile)
+    hdr=hdu1[0].header #the header associated with extension=0
+
+    #create/update luminosity function parameters in model file header
+    hdr.set('PHI0',value_initial[0],'Luminosity Function Normalization')
+    hdr.set('PHI0_FIX',value_fix[0],'Fix Phi0 (Y=1/N=0)')
+    hdr.set('PHI0_MIN',value_min[0],'Minimum Phi0 value')
+    hdr.set('PHI0_MAX',value_max[0],'Maximum Phi0 value')
+    hdr.set('PHI0_DP',value_dp[0],'sigma phi0')
+    
+    hdr.set('L0',value_initial[1],'Luminosity Function knee')
+    hdr.set('L0_FIX',value_fix[1],'Fix L0 (Y=1/N=0)')
+    hdr.set('L0_MIN',value_min[1],'Minimum L0 value')
+    hdr.set('L0_MAX',value_max[1],'Maximum L0 value')
+    hdr.set('L0_DP',value_dp[0],'sigma L0')
+ 
+    hdr.set('ALPHA',value_initial[2],'Luminosity Function upper slope')
+    hdr.set('ALPHA_FI',value_fix[2],'Fix alpha (Y=1/N=0)')
+    hdr.set('ALPHA_MI',value_min[2],'Minimum alpha value')
+    hdr.set('ALPHA_MA',value_max[2],'Maximum alpha value')
+    hdr.set('ALPHA_DP',value_dp[2],'sigma alpha')
+
+    hdr.set('BETA',value_initial[3],'Luminosity Function lower slope')
+    hdr.set('BETA_FIX',value_fix[3],'Fix beta (Y=1/N=0)')
+    hdr.set('BETA_MIN',value_min[3],'Minimum beta value')
+    hdr.set('BETA_MAX',value_max[3],'Maximum beta value')
+    hdr.set('BETA_DP',value_dp[3],'sigma beta')
+
+    hdr.set('P',value_initial[4],'LF density evolution parameter')
+    hdr.set('P_FIX',value_fix[4],'Fix p (Y=1/N=0)')
+    hdr.set('P_MIN',value_min[4],'Minimum p value')
+    hdr.set('P_MAX',value_max[4],'Maximum p value')
+    hdr.set('P_DP',value_dp[4],'sigma p')
+
+    hdr.set('Q',value_initial[5],'LF luminosity evolution parameter')
+    hdr.set('Q_FIX',value_fix[5],'Fix q (Y=1/N=0)')
+    hdr.set('Q_MIN',value_min[5],'Minimum q value')
+    hdr.set('Q_MAX',value_max[5],'Maximum q value')
+    hdr.set('Q_DP',value_dp[5],'sigma q')
+    
+    hdr.set('ZCUT',value_initial[6],'LF evolution redshift cutoff')
+    hdr.set('ZCUT_FIX',value_fix[6],'Fix zcut (Y=1/N=0)')
+    hdr.set('ZCUT_MIN',value_min[6],'Minimum zcut value')
+    hdr.set('ZCUT_MAX',value_max[6],'Maximum zcut value')
+    hdr.set('ZCUT_DP',value_max[6],'sigma zcut')
+
+    hdr.set('CEXP',value_initial[6],'Color evolution term')
+    hdr.set('CEXP_FIX',value_fix[6],'Fix cexp (Y=1/N=0)')
+    hdr.set('CEXP_MIN',value_min[6],'Minimum cexp value')
+    hdr.set('CEXP_MAX',value_max[6],'Maximum cexp value')
+    hdr.set('CEXP_DP',value_dp[6],'sigma cexp')
+    
+    hdr.set('AREA',area[0],'Observed Solid Angle of survey')
+  
+    #these aren't currently user controllable in widget, but can change them right here at own risk  
+    hdr.set('ZMIN',0.0,'Simulation minimum redshift')
+    hdr.set('ZMAX',5.0,'Simulation maximum redshift')
+    hdr.set('DZ',0.1,'Redshift Bin Width')
+    hdr.set('RUNS',1.e3,'Number of runs')
+
+    hdr.set('NCHAIN',5,'Chain Number')
+    hdr.set('TMAX',20.0,'Starting Anneal Temperature')
+    hdr.set('ANN_PCT',0.25,'Ideal acceptance Percentage')
+    hdr.set('ANN_RNG',0.05,'Range from ideal within which to maintainacceptance')
+    hdr.set('CONV_CON',0.5,'Convergence confidence interval') #was 0.05 but having trouble converging so set to 0.5 just for now
+    hdr.set('CONV_RMA',1.05,'Convergence Rmax Criterion')
+    hdr.set('CONV_STE',20,'Iterations between convergence checks')
+    hdr.set('BURN_STE',10,'Iterations between anneal calls in burn-in')
+    hdr.set('BURNVRUN',10,'Ratio of normal to burn-in steps')
+    hdr.set('PRINT',1,'Whether to Print Debug MSGs (1=verbose,0=silent)')
+
+    hdr['HISTORY']='Last updated on: '+time.strftime("%c") #get current date+time
+    hdu1.close
+
+#this is all to do with the GUI where the user is allowed to change the model parameter values etc
 if (mdefaults == 'y'):
     import Tkinter
     from functools import partial
@@ -153,95 +237,25 @@ if (mdefaults == 'y'):
         flim[0]=f1.get()
         flim[1]=f2.get()
         flim[2]=f3.get()
+        update_mfile(modelfile)
 
     if __name__ == '__main__':
-        root = Tk()
         ents = makeform(labelframe, fields)
         root.bind('<Return>', partial(fetch, ents))
         ents2 = makeform2(labelframe2, fields2)
         root.bind('<Return>', partial(fetch, ents2))
         b1 = Button(labelframe2, text='Update',command=callback)
         b1.pack(side=LEFT,padx=5,pady=5)
-        b2 = Button(labelframe2, text='Quit', command=root.quit)
-        b2.pack(side=LEFT, padx=5, pady=5)
+        b2 = Button(labelframe2, text='Run',command=runcode)
+        b2.pack(side=LEFT,padx=5,pady=5)
+        b3 = Button(labelframe2, text='Show results',command=showresults)
+        b3.pack(side=LEFT,padx=5,pady=5)
+        b4 = Button(labelframe2, text='Quit', command=root.quit)
+        b4.pack(side=LEFT, padx=5, pady=5)
         root.mainloop() 
 
-#call actual fitter code
-torun=raw_input("Run code (y/n)?");
-
-hdu1=fits.open(modelfile)
-hdr=hdu1[0].header #the header associated with extension=0
-
-#create/update luminosity function parameters in model file header
-hdr.set('PHI0',value_initial[0],'Luminosity Function Normalization')
-hdr.set('PHI0_FIX',value_fix[0],'Fix Phi0 (Y=1/N=0)')
-hdr.set('PHI0_MIN',value_min[0],'Minimum Phi0 value')
-hdr.set('PHI0_MAX',value_max[0],'Maximum Phi0 value')
-hdr.set('PHI0_DP',value_dp[0],'sigma phi0')
-    
-hdr.set('L0',value_initial[1],'Luminosity Function knee')
-hdr.set('L0_FIX',value_fix[1],'Fix L0 (Y=1/N=0)')
-hdr.set('L0_MIN',value_min[1],'Minimum L0 value')
-hdr.set('L0_MAX',value_max[1],'Maximum L0 value')
-hdr.set('L0_DP',value_dp[0],'sigma L0')
- 
-hdr.set('ALPHA',value_initial[2],'Luminosity Function upper slope')
-hdr.set('ALPHA_FI',value_fix[2],'Fix alpha (Y=1/N=0)')
-hdr.set('ALPHA_MI',value_min[2],'Minimum alpha value')
-hdr.set('ALPHA_MA',value_max[2],'Maximum alpha value')
-hdr.set('ALPHA_DP',value_dp[2],'sigma alpha')
-
-hdr.set('BETA',value_initial[3],'Luminosity Function lower slope')
-hdr.set('BETA_FIX',value_fix[3],'Fix beta (Y=1/N=0)')
-hdr.set('BETA_MIN',value_min[3],'Minimum beta value')
-hdr.set('BETA_MAX',value_max[3],'Maximum beta value')
-hdr.set('BETA_DP',value_dp[3],'sigma beta')
-
-hdr.set('P',value_initial[4],'Luminosity Function density evolution parameter')
-hdr.set('P_FIX',value_fix[4],'Fix p (Y=1/N=0)')
-hdr.set('P_MIN',value_min[4],'Minimum p value')
-hdr.set('P_MAX',value_max[4],'Maximum p value')
-hdr.set('P_DP',value_dp[4],'sigma p')
-
-hdr.set('Q',value_initial[5],'Luminosity Function luminosity evolution parameter')
-hdr.set('Q_FIX',value_fix[5],'Fix q (Y=1/N=0)')
-hdr.set('Q_MIN',value_min[5],'Minimum q value')
-hdr.set('Q_MAX',value_max[5],'Maximum q value')
-hdr.set('Q_DP',value_dp[5],'sigma q')
-    
-hdr.set('ZCUT',value_initial[6],'Luminosity Function evolution redshift cutoff')
-hdr.set('ZCUT_FIX',value_fix[6],'Fix zcut (Y=1/N=0)')
-hdr.set('ZCUT_MIN',value_min[6],'Minimum zcut value')
-hdr.set('ZCUT_MAX',value_max[6],'Maximum zcut value')
-hdr.set('ZCUT_DP',value_max[6],'sigma zcut')
-
-hdr.set('CEXP',value_initial[6],'Color evolution term')
-hdr.set('CEXP_FIX',value_fix[6],'Fix cexp (Y=1/N=0)')
-hdr.set('CEXP_MIN',value_min[6],'Minimum cexp value')
-hdr.set('CEXP_MAX',value_max[6],'Maximum cexp value')
-hdr.set('CEXP_DP',value_dp[6],'sigma cexp')
-    
-hdr.set('AREA',area[0],'Observed Solid Angle of survey')
-  
-#these aren't currently user controllable in widget, but can change them right here at own risk  
-hdr.set('ZMIN',0.0,'Simulation minimum redshift')
-hdr.set('ZMAX',5.0,'Simulation maximum redshift')
-hdr.set('DZ',0.1,'Redshift Bin Width')
-hdr.set('RUNS',1.e3,'Number of runs')
-
-hdr.set('NCHAIN',5,'Chain Number')
-hdr.set('TMAX',20.0,'Starting Anneal Temperature')
-hdr.set('ANN_PCT',0.25,'Ideal acceptance Percentage')
-hdr.set('ANN_RNG',0.05,'Range from ideal within which to maintainacceptance')
-hdr.set('CONV_CON',0.05,'Convergence CI Setting')
-hdr.set('CONV_RMA',1.05,'Convergence Rmax Criterion')
-hdr.set('CONV_STE',20,'Iterations between convergence checks')
-hdr.set('BURN_STE',10,'Iterations between anneal calls in burn-in')
-hdr.set('BURNVRUN',10,'Ratio of normal to burn-in steps')
-hdr.set('PRINT',1,'Whether to Print Debug MSGs (1=verbose,0=silent)')
-
-hdr['HISTORY']='Last updated on: '+time.strftime("%c") #get current date+time
-hdu1.close
-
-if (torun == 'y'):
-    os.system("./Fitting/v0/fitter"+' '+obsfile+' '+modelfile+' '+sedfile+' '+outfile)
+#in case don't want to see GUI, use command line to run the code and display results
+if (mdefaults == 'n'):
+    torun=raw_input("Run code (y/n)?");
+    if (torun == 'y'):
+        runcode()
