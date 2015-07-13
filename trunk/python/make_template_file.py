de09a4ab8a316f5929683dcb59d7af68ad1997c7
asajina <annie.sajina@gmail.com>
2015-07-13 09:42:24 -0400

    new default_templates.fits

--- trunk/python/make_template_file.py
+++ trunk/python/make_template_file.py
@@ -29,7 +29,7 @@ hdulist = fits.HDUList([hdu])
 
 lir_list=[9.75,10.0,10.25,10.50,10.75,11.00,11.25,11.50,11.75,12.00,12.25,12.50,12.75,13.00]
 
-sedfile=seddir+'/Kirkpatrick2015_templates.fits'
+sedfile=os.getcwd()+'/trunk/templates/default_templates.fits'
 
 #============================================================================
 #SFR galaxies
@@ -76,6 +76,7 @@ dnu_default=(np.abs(lam_default-np.roll(lam_default,1))/lam_default)/lam_default
 dnu_default=(c*1e6)*dnu_default
 
 #read in Sc spiral galaxy template from SWIRE template library in order to get the stellar bump and switch to the above at 0.5-4um
+#use the Sc template for the low-L cases (logL<11.7)
 with open ('/Users/annie/code/idl/auxdata/swire/Sc_template_norm.sed','r') as f:
     flines=f.readlines()
     fcount=-1
@@ -84,12 +85,29 @@ with open ('/Users/annie/code/idl/auxdata/swire/Sc_template_norm.sed','r') as f:
         tmp0,tmp1=fline.split()
         lam_swire.append(float(tmp0)),flam_swire.append(float(tmp1))
 
+#use the IRAS20551 starburst template for the higher-L cases
+with open ('/Users/annie/code/idl/auxdata/swire/I20551_template_norm.sed','r') as f:
+    flines=f.readlines()
+    fcount=-1
+    lam_swire_ulirg,flam_swire_ulirg=[],[]
+    for fline in flines:
+        tmp0,tmp1=fline.split()
+        lam_swire_ulirg.append(float(tmp0)),flam_swire_ulirg.append(float(tmp1))
+
 lam_swire=np.array(lam_swire)/1e4 #convert to microns
 lnu_swire=np.array(flam_swire)*lam_swire**2. #convert to fnu but overall normalization not set yet!
- 
+
+lam_swire_ulirg=np.array(lam_swire_ulirg)/1e4 #convert to microns
+lnu_swire_ulirg=np.array(flam_swire_ulirg)*lam_swire_ulirg**2. #convert to fnu but overall normalization not set yet!
+
+f_tmp=interpolate.interp1d(lam_swire_ulirg,lnu_swire_ulirg)
+lnu_swire_ulirg_interp=f_tmp(lam_swire)
+
 f = interpolate.interp1d(lam_swire,lnu_swire)
+f_ulirg=interpolate.interp1d(lam_swire,lnu_swire_ulirg_interp)
 #print lam_swire[0],lam_swire[1196]
 lnu_tmp=f(lam_short)
+lnu_tmp_ulirg=f_ulirg(lam_short)
 istitch=45
 #print lam_short[istitch],lnu_tmp[istitch],lam_rieke[0],lnu1[0]
 tmp=lnu_tmp*(lnu1[0]/lnu_tmp[istitch])
@@ -106,22 +124,22 @@ tmp=lnu_tmp*(lnu6[0]/lnu_tmp[istitch])
 lnu6_default=np.concatenate((tmp,lnu6),axis=0)
 tmp=lnu_tmp*(lnu7[0]/lnu_tmp[istitch])
 lnu7_default=np.concatenate((tmp,lnu7),axis=0)
-tmp=lnu_tmp*(lnu8[0]/lnu_tmp[istitch])
-lnu8_default=np.concatenate((tmp,lnu8),axis=0)
-tmp=lnu_tmp*(lnu9[0]/lnu_tmp[istitch])
-lnu9_default=np.concatenate((tmp,lnu9),axis=0)
-tmp=lnu_tmp*(lnu10[0]/lnu_tmp[istitch])
-lnu10_default=np.concatenate((tmp,lnu10),axis=0)
-tmp=lnu_tmp*(lnu11[0]/lnu_tmp[istitch])
-lnu11_default=np.concatenate((tmp,lnu11),axis=0)
-tmp=lnu_tmp*(lnu12[0]/lnu_tmp[istitch])
-lnu12_default=np.concatenate((tmp,lnu12),axis=0)
-tmp=lnu_tmp*(lnu13[0]/lnu_tmp[istitch])
-lnu13_default=np.concatenate((tmp,lnu13),axis=0)
-tmp=lnu_tmp*(lnu14[0]/lnu_tmp[istitch])
-lnu14_default=np.concatenate((tmp,lnu14),axis=0)
 
-#print lam_default[0]
+#from here on use the ulirg-based near-IR SED
+tmp=lnu_tmp_ulirg*(lnu8[0]/lnu_tmp_ulirg[istitch])
+lnu8_default=np.concatenate((tmp,lnu8),axis=0)
+tmp=lnu_tmp_ulirg*(lnu9[0]/lnu_tmp_ulirg[istitch])
+lnu9_default=np.concatenate((tmp,lnu9),axis=0)
+tmp=lnu_tmp_ulirg*(lnu10[0]/lnu_tmp_ulirg[istitch])
+lnu10_default=np.concatenate((tmp,lnu10),axis=0)
+tmp=lnu_tmp_ulirg*(lnu11[0]/lnu_tmp_ulirg[istitch])
+lnu11_default=np.concatenate((tmp,lnu11),axis=0)
+tmp=lnu_tmp_ulirg*(lnu12[0]/lnu_tmp_ulirg[istitch])
+lnu12_default=np.concatenate((tmp,lnu12),axis=0)
+tmp=lnu_tmp_ulirg*(lnu13[0]/lnu_tmp_ulirg[istitch])
+lnu13_default=np.concatenate((tmp,lnu13),axis=0)
+tmp=lnu_tmp_ulirg*(lnu14[0]/lnu_tmp_ulirg[istitch])
+lnu14_default=np.concatenate((tmp,lnu14),axis=0)
 
 dnu=(np.abs(lam_default-np.roll(lam_default,1))/lam_default)/lam_default 
 dnu=(c*1e6)*dnu
@@ -174,7 +192,6 @@ scale12=pow(10,lir_list[11]-np.log10(lir12))
 scale13=pow(10,lir_list[12]-np.log10(lir13))
 scale14=pow(10,lir_list[13]-np.log10(lir14))
 
-print scale1,scale2,scale3,scale4,scale5,scale13
 col1=fits.Column(name='lambda',format='FLOAT',array=lam_default)
 col2=fits.Column(name='lnu1',format='FLOAT',array=lnu1_default*scale1)
 col3=fits.Column(name='lnu2',format='FLOAT',array=lnu2_default*scale2)
@@ -195,7 +212,7 @@ col15=fits.Column(name='lnu14',format='FLOAT',array=lnu14_default*scale14)
 #plt.xscale('log')
 #plt.yscale('log')
 #plt.show()
-print len(lam_default),len(lnu1_default)
+#print len(lam_default),len(lnu1_default)
 
 cols_SFG_z0=fits.ColDefs([col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15])
 
@@ -230,7 +247,7 @@ else:
     lnu_old=np.concatenate((lnu_old,[0]),axis=0)
 
 f = interpolate.interp1d(lam_old,lnu_old)
-print lam_old[0],lam_default[0]
+#print lam_old[0],lam_default[0]
 lnu=f(lam_default)
 
 dnu=(np.abs(lam_default-np.roll(lam_default,1))/lam_default)/lam_default 
@@ -242,13 +259,24 @@ for i in range(0,len(lam_default)-1):
 
 print 'SFG1 template L_TIR (after interp)', np.log10(lir)
 
-col2=fits.Column(name='lnu1',format='FLOAT',array=lnu*scale[0])
-col3=fits.Column(name='lnu2',format='FLOAT',array=lnu*scale[1])
-col4=fits.Column(name='lnu3',format='FLOAT',array=lnu*scale[2])
-col5=fits.Column(name='lnu4',format='FLOAT',array=lnu*scale[3])
-col6=fits.Column(name='lnu5',format='FLOAT',array=lnu*scale[4])
-col7=fits.Column(name='lnu6',format='FLOAT',array=lnu*scale[5])
-col8=fits.Column(name='lnu7',format='FLOAT',array=lnu*scale[6])
+#this is if we scale the existing SFG1 template from Kirkpatrick+2015 down to logL=9.75
+#col2=fits.Column(name='lnu1',format='FLOAT',array=lnu*scale[0])
+#col3=fits.Column(name='lnu2',format='FLOAT',array=lnu*scale[1])
+#col4=fits.Column(name='lnu3',format='FLOAT',array=lnu*scale[2])
+#col5=fits.Column(name='lnu4',format='FLOAT',array=lnu*scale[3])
+#col6=fits.Column(name='lnu5',format='FLOAT',array=lnu*scale[4])
+#col7=fits.Column(name='lnu6',format='FLOAT',array=lnu*scale[5])
+#col8=fits.Column(name='lnu7',format='FLOAT',array=lnu*scale[6])
+
+#this is if we assume that the z=0 templates for low-L galaxies are constant
+col2=fits.Column(name='lnu1',format='FLOAT',array=lnu1_default*scale1)
+col3=fits.Column(name='lnu2',format='FLOAT',array=lnu2_default*scale2)
+col4=fits.Column(name='lnu3',format='FLOAT',array=lnu3_default*scale3)
+col5=fits.Column(name='lnu4',format='FLOAT',array=lnu4_default*scale4)
+col6=fits.Column(name='lnu5',format='FLOAT',array=lnu5_default*scale5)
+col7=fits.Column(name='lnu6',format='FLOAT',array=lnu6_default*scale6)
+col8=fits.Column(name='lnu7',format='FLOAT',array=lnu7_default*scale7)
+
 col9=fits.Column(name='lnu8',format='FLOAT',array=lnu*scale[7])
 col10=fits.Column(name='lnu9',format='FLOAT',array=lnu*scale[8])
 
@@ -321,13 +349,24 @@ else:
 f = interpolate.interp1d(lam_old,lnu_old)
 lnu=f(lam_default)
 
-col2=fits.Column(name='lnu1',format='FLOAT',array=lnu*scale[0])
-col3=fits.Column(name='lnu2',format='FLOAT',array=lnu*scale[1])
-col4=fits.Column(name='lnu3',format='FLOAT',array=lnu*scale[2])
-col5=fits.Column(name='lnu4',format='FLOAT',array=lnu*scale[3])
-col6=fits.Column(name='lnu5',format='FLOAT',array=lnu*scale[4])
-col7=fits.Column(name='lnu6',format='FLOAT',array=lnu*scale[5])
-col8=fits.Column(name='lnu7',format='FLOAT',array=lnu*scale[6])
+#scaling from Kirkpatrick+2015 SFGz2
+#col2=fits.Column(name='lnu1',format='FLOAT',array=lnu*scale[0])
+#col3=fits.Column(name='lnu2',format='FLOAT',array=lnu*scale[1])
+#col4=fits.Column(name='lnu3',format='FLOAT',array=lnu*scale[2])
+#col5=fits.Column(name='lnu4',format='FLOAT',array=lnu*scale[3])
+#col6=fits.Column(name='lnu5',format='FLOAT',array=lnu*scale[4])
+#col7=fits.Column(name='lnu6',format='FLOAT',array=lnu*scale[5])
+#col8=fits.Column(name='lnu7',format='FLOAT',array=lnu*scale[6])
+
+#this is if we assume that the z=0 templates for low-L galaxies are constant
+col2=fits.Column(name='lnu1',format='FLOAT',array=lnu1_default*scale1)
+col3=fits.Column(name='lnu2',format='FLOAT',array=lnu2_default*scale2)
+col4=fits.Column(name='lnu3',format='FLOAT',array=lnu3_default*scale3)
+col5=fits.Column(name='lnu4',format='FLOAT',array=lnu4_default*scale4)
+col6=fits.Column(name='lnu5',format='FLOAT',array=lnu5_default*scale5)
+col7=fits.Column(name='lnu6',format='FLOAT',array=lnu6_default*scale6)
+col8=fits.Column(name='lnu7',format='FLOAT',array=lnu7_default*scale7)
+
 col9=fits.Column(name='lnu8',format='FLOAT',array=lnu*scale[7])
 col10=fits.Column(name='lnu9',format='FLOAT',array=lnu*scale[8])
 col11=fits.Column(name='lnu10',format='FLOAT',array=lnu*scale[9])
@@ -352,6 +391,47 @@ plt.show()
 #============================================================================
 #AGN
 #----------------------------------------------------------------------------
+
+#for z=0 use Mullaney et al template. 
+mullaney_filename='/Users/annie/code/idl/auxdata/mullaney_table3.tex'
+with open (mullaney_filename,'r') as f:
+    flines=f.readlines()[6:]
+    agn_z0_lam,agn_z0_lnu=[],[]
+    for fline in flines:
+        tmp1,tmp2,tmp3,tmp4,tmp5,tmp6,tmp7,tmp8,tmp9,tmp10,tmp11=fline.split()
+        agn_z0_lam.append(float(tmp1)),agn_z0_lnu.append(float(tmp2))
+
+
+f = interpolate.interp1d([lam_default[0],agn_z0_lam],[0,agn_z0_lnu])
+lnu=f(lam_default)
+#these should be the same as above when using the same lambda
+#dnu=(np.abs(lam-np.roll(lam,1))/lam)/lam 
+#dnu=(c*1e6)*dnu
+lir=0
+for i in range(0,len(lam_default)-1):
+    if(lam_default[i] >= 5.0):
+        lir+=(lnu[i]*dnu[i])/3.86e26
+
+print 'AGN0 template L_TIR', np.log10(lir)
+scale=pow(10,lir_list-np.log10(lir))
+
+col2=fits.Column(name='lnu1',format='FLOAT',array=lnu*scale[0])
+col3=fits.Column(name='lnu2',format='FLOAT',array=lnu*scale[1])
+col4=fits.Column(name='lnu3',format='FLOAT',array=lnu*scale[2])
+col5=fits.Column(name='lnu4',format='FLOAT',array=lnu*scale[3])
+col6=fits.Column(name='lnu5',format='FLOAT',array=lnu*scale[4])
+col7=fits.Column(name='lnu6',format='FLOAT',array=lnu*scale[5])
+col8=fits.Column(name='lnu7',format='FLOAT',array=lnu*scale[6])
+col9=fits.Column(name='lnu8',format='FLOAT',array=lnu*scale[7])
+col10=fits.Column(name='lnu9',format='FLOAT',array=lnu*scale[8])
+col11=fits.Column(name='lnu10',format='FLOAT',array=lnu*scale[9])
+col12=fits.Column(name='lnu11',format='FLOAT',array=lnu*scale[10])
+col13=fits.Column(name='lnu12',format='FLOAT',array=lnu*scale[11])
+col14=fits.Column(name='lnu13',format='FLOAT',array=lnu*scale[12])
+col15=fits.Column(name='lnu14',format='FLOAT',array=lnu*scale[13])
+
+
+cols_AGN_z0=fits.ColDefs([col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15])
 
 with open (seddir+'/Comprehensive_AGN1.txt','r') as f: 
     flines=f.readlines()[4:] #4 lines of header, and 4 lines as the lower-lambda values not well defined for the higher-L templates
@@ -541,6 +621,8 @@ tbhdu11.header.update('EXTNAME','SFG_z1',
 tbhdu12.header.update('EXTNAME','SFG_z2',
                         'Kirkpatrick2015')
 
+tbhdu10.header.update('EXTNAME','AGN_z0',
+                        'Mullaney2011')
 tbhdu21.header.update('EXTNAME','AGN_z1',
                         'Kirkpatrick2015')
 tbhdu22.header.update('EXTNAME','AGN_z2',
@@ -549,6 +631,7 @@ tbhdu22.header.update('EXTNAME','AGN_z2',
 tbhdr=tbhdu10.header
 tbhdr=tbhdu11.header
 tbhdr=tbhdu12.header
+tbhdr=tbhdu10.header
 tbhdr=tbhdu21.header
 tbhdr=tbhdu22.header
 
@@ -585,7 +668,7 @@ hdulist[0].header.update('L14',lir_list[13],'luminosity array')
 hdr=hdulist[0].header
 
 hdulist.close
-thdulist=fits.HDUList([hdulist[0],tbhdu10,tbhdu11,tbhdu12,tbhdu21,tbhdu22])
+thdulist=fits.HDUList([hdulist[0],tbhdu10,tbhdu11,tbhdu12,tbhdu10,tbhdu21,tbhdu22])
 if(os.path.isfile(sedfile)):
     print 'Replacing existing template file...'
     os.remove(sedfile)
