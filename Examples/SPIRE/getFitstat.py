da2d57a31b0778634adc17e62c8fe5ac7d5ce0ce
Noah Kurinsky <nkurinsky91@gmail.com>
2015-09-16 10:25:41 -0400

    added checks for headers in configure

--- Examples/SPIRE/getFitstat.py
+++ Examples/SPIRE/getFitstat.py
@@ -14,14 +14,15 @@ if (len(sys.argv) < 2):
     print "Please provide model file name"
     quit()
 
+print "{0:<35} {1} {2}".format("Model","ChiSquare","Likelihood")
 for fname in sys.argv[1:]:
     output=OutputFile(fname)
     model=fname.split('_output')[0]
     field=fname.split('_')[1]
     if(field == "COSMOS"):
-        best=0.14
+        best=0.1422
     elif(field == "Lockman-SWIRE"):
-        best=0.16
+        best=0.1678
     else:
         raise ValueError("Unknown Field "+field)
-    print "{0:<35}{1:.3}".format(model,numpy.exp(best-output.simInfo.fitstat))
+    print "{0:<35} {1:.4} {2:.3}".format(model,output.simInfo.fitstat,numpy.exp(best-output.simInfo.fitstat))
