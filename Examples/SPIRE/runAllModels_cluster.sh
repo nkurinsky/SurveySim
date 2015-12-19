b25309e185cb46ddfb4dd654bd9c86df995bf005
Noah Kurinsky <nkurinsky91@gmail.com>
2015-12-19 12:50:03 -0800

    adding MIPS output

--- Examples/SPIRE/runAllModels_cluster.sh
+++ Examples/SPIRE/runAllModels_cluster.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-for field in "0 2"
+fields="2"
+
+for field in $fields
 do
-    for lf in "0 1"
+    for lf in {0..1}
     do
 	for model in {0..2}
 	do
 	    echo "./run_analysis_cluster.py $field $model $lf"
-	    bsub -q long ./run_analysis_cluster.py $field $model $lf
+	    bsub -W 960 ./run_analysis_cluster.py $field $model $lf
 	done
     done
 done
